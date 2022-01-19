c45672aa5ad2bfc350e0977b35b7655463339a6c
Harsh Mamania <86950272+mamaniaharsh7@users.noreply.github.com>
2022-01-19 19:43:51 +0530

    SQL Queries used

--- Revenue Analysis/Queries__RevenueAnalysis.sql
+++ Revenue Analysis/Queries__RevenueAnalysis.sql
@@ -54,7 +54,7 @@ ORDER BY year(order_date)  ;
     
 2. This implies, 
     from 2017 (estimated 37Cr) to 2018 (41.4Cr) year-on-year growth 	--> 0.11346 (11% incline)
-    from 2018 (41.4Cr) to 2019 (33.6Cr) year-on-year growth 			--> -0.18774 (19% decline)
+    from 2018 (41.4Cr) to 2019 (33.6Cr) year-on-year growth 		--> -0.18774 (19% decline)
     from 2019 (33.6Cr) to 2020 (estimated 28.4Cr) year-on-year growth 	--> -0.15347 (15% decline)
     
 3. which means, on an Average, (-0.07591) '7.5% decline' in Revenue every year.
@@ -68,17 +68,17 @@ GROUP BY year(order_date), quarter(order_date)
 ORDER BY Yearr, Quarterr ;
 /*
 	Yearr	Quarterr	Quarterly_Revenue	Quarterly_Revenue_Change	Sales_Qty
-	2017		4			92882653				NULL				 234462
-	2018		1			115917613				24.80				 265793
-	2018		2			102824550			    -11.29				 271037
-	2018		3			105531222				2.63				 246481
-	2018		4			89413778				-15.27				 214186
-	2019		1			86932142				-2.77				 203795
-	2019		2			81027451				-6.79				 195263
-	2019		3			92181694				13.76				 248187
-	2019		4			75877815				-17.68				 199838
-	2020		1			78965343				4.06				 204413
-	2020		2			63259202				-19.88				 145827
+	2017	   4		     92882653			NULL			 234462
+	2018	   1		     115917613			24.80			 265793
+	2018	   2		     102824550			-11.29			 271037
+	2018	   3		     105531222			2.63			 246481
+	2018	   4		     89413778			-15.27			 214186
+	2019	   1		     86932142			-2.77			 203795
+	2019	   2		     81027451			-6.79			 195263
+	2019	   3		     92181694			13.76			 248187
+	2019	   4		     75877815			-17.68			 199838
+	2020	   1		     78965343			4.06			 204413
+	2020	   2		     63259202			-19.88			 145827
 
 <<could've applied the same query  logic to directly calculate the Y-o-y growth in previous query, but the Dataset had only 3 months data for 2017, and 6 months data for 2020, which would've hampered the accuracy of calculation.
 */
@@ -120,7 +120,7 @@ FROM cte_tbl
 WHERE Rnk<6 ;
 /*
 	Thus, we see that the Top 5 Revenue Generating Cities have nearly remained the same (and in the same order as well) throughout the Years.
-    1	Delhi NCR
+    	1	Delhi NCR
 	2	Mumbai
 	3	Ahmedabad
 	4	Nagpur
@@ -144,10 +144,11 @@ GROUP BY year(order_date)
 ORDER BY Yearr ;
 /*
 	Yearr	Types of Products sold
-	2017	208
-	2018	264
-	2019	238
-	2020	197
+	2017		208
+	2018		264
+	2019		238
+	2020		197
+
 1. So, out of the '279' total types of products in the inventory, not all types have been sold each year... there's been lot of fluctuations!
 
 2. (Essentially, it appears that with every passing year, lot of products have gone unsold/been discontinued... 
@@ -196,17 +197,17 @@ ORDER BY (selling_price/sales_qty)
 
 /*
 	Price_Range		No_of_Unique_Products_in_this_Range	Revenue_Generated_from_this_Range	Percentage_contribution_to_Revenue
-	0 to 500					298							267066724									27.1185
-	500 to 1000					208							188650023									19.1559
-	1000 to 1500				138							206595481									20.9781
-	1500 to 2000				101							153620605									15.599
-	2000 to 2500				63							87150545									8.8494
-	2500 to 3000				35							27710999									2.8138
-	3000 to 3500				21							14573147									1.4798
-	3500 to 4000				14							10425999									1.0587
-	4000 to 4500				10							28985816									2.9433
-	4500 to 5000				3							18976										0.0019
-	5000 to 5500				1							15148										0.0015
+	0 to 500				298				   267066724				    27.1185
+	500 to 1000				208				   188650023				    19.1559
+	1000 to 1500				138				   206595481				    20.9781
+	1500 to 2000				101				   153620605				    15.599
+	2000 to 2500				63				   87150545				    8.8494
+	2500 to 3000				35				   27710999				    2.8138
+	3000 to 3500				21				   14573147				    1.4798
+	3500 to 4000				14				   10425999				    1.0587
+	4000 to 4500				10				   28985816				    2.9433
+	4500 to 5000				3				   18976				    0.0019
+	5000 to 5500				1				   15148				    0.0015
 
 1. Thus, we see that the most 'economical' price ranges contribute the most to the Revenue!!
 	--> As, the we keep going up in the 'price range', their contribution to the Revenue keeps going down. (Inversely related)
@@ -251,10 +252,10 @@ GROUP BY YEARR ;
 <<Here, i've used a 'TempTable' just for the sake of practice... could've easily done this using CTE, View or SubQuery also.>>
 
 	yearr	Total_SalesQty	SalesQty_OWN_BRAND	Percent_of_Total_SalesQty	SalesQty_DISTRIBUTION	Percent_of_Total_SalesQty	Revenue_from_OWN_BRAND	Revenue_from_DISTRIBUTION
-	2017		192730			161913					84.0103						30817					15.9897						34272017				9706266
-	2018		819210			665248					81.2060						153962					18.7940						161756295				51317068
-	2019		706455			495559					70.1473						210896					29.8527						126207845				55198050
-	2020		291468			190567					65.3818						100901					34.6182						47576491				29818192
+	2017	   192730	    161913			84.0103				30817			15.9897				34272017		9706266
+	2018	   819210	    665248			81.2060				153962			18.7940				161756295		51317068
+	2019	   706455	    495559			70.1473				210896			29.8527				126207845		55198050
+	2020	   291468	    190567			65.3818				100901			34.6182				47576491		29818192
 
 1. Over the years, we can say that, out of '100' products sold, on an Average '75' would be 'Own Brand' and '25' would be of 'Distribution' Type.
 
@@ -280,29 +281,29 @@ FROM cte_prodRank
 WHERE Rnk<6 ;
 /*
 	Yearr	Rnk	product_code	product_type	Revenue		Contribution_to_Total_Revenue_that_year
-	2017	1	Prod099			Own Brand		4176622						9.5
-	2017	2	Prod239			Own Brand		1681827						3.82
-	2017	3	Prod105			Own Brand		1597217						3.63
-	2017	4	Prod209			Own Brand		1459093						3.32
-	2017	5	Prod053			Own Brand		1415754						3.22
+	2017	1	Prod099		Own Brand	4176622				9.5
+	2017	2	Prod239		Own Brand	1681827				3.82
+	2017	3	Prod105		Own Brand	1597217				3.63
+	2017	4	Prod209		Own Brand	1459093				3.32
+	2017	5	Prod053		Own Brand	1415754				3.22
     
-	2018	1	Prod040			Own Brand		13419987					6.3
-	2018	2	Prod159			Distribution	10865231					5.1
-	2018	3	Prod053			Own Brand		7977828						3.74
-	2018	4	Prod049			Own Brand		7306890						3.43
-	2018	5	Prod239			Own Brand		7218613						3.39
+	2018	1	Prod040		Own Brand	13419987			6.3
+	2018	2	Prod159		Distribution	10865231			5.1
+	2018	3	Prod053		Own Brand	7977828				3.74
+	2018	4	Prod049		Own Brand	7306890				3.43
+	2018	5	Prod239		Own Brand	7218613				3.39
 	
-    2019	1	Prod018			Own Brand		8281419						4.57
-	2019	2	Prod040			Own Brand		7750278						4.27
-	2019	3	Prod065			Own Brand		6814949						3.76
-	2019	4	Prod053			Own Brand		5564681						3.07
-	2019	5	Prod090			Own Brand		5253110						2.9
+        2019	1	Prod018		Own Brand	8281419				4.57
+	2019	2	Prod040		Own Brand	7750278				4.27
+	2019	3	Prod065		Own Brand	6814949				3.76
+	2019	4	Prod053		Own Brand	5564681				3.07
+	2019	5	Prod090		Own Brand	5253110				2.9
     
-	2020	1	Prod047			Own Brand		3982487						5.15
-	2020	2	Prod061			Own Brand		3503835						4.53
-	2020	3	Prod071			Distribution	3365872						4.35
-	2020	4	Prod065			Own Brand		3135964						4.05
-	2020	5	Prod237			Distribution	2724291						3.52
+	2020	1	Prod047		Own Brand	3982487				5.15
+	2020	2	Prod061		Own Brand	3503835				4.53
+	2020	3	Prod071		Distribution	3365872				4.35
+	2020	4	Prod065		Own Brand	3135964				4.05
+	2020	5	Prod237		Distribution	2724291				3.52
     
 Thus, we see that :
 (There's no clear winner !)
@@ -375,10 +376,10 @@ FROM Transactions_and_Customers_temp
 GROUP BY yearr ;
 /*
 	yearr	Total_SalesQty	SalesQty_BRICK&MORTAR	Percent_of_Total_SalesQty	SalesQty_E-COMMERCE		Percent_of_Total_SalesQty	Revenue_from_BRICK&MORTAR	Revenue_from_E-COMMERCE
-	2017		234462				181689					77.4919						52773					22.5081						66020915					26861738
-	2018		997497				759078					76.0983						238419					23.9017						307748862					105938301
-	2019		847083				651750					76.9405						195333					23.0595						257563281					78455821
-	2020		350240				261684					74.7156						88556					25.2844						113136780					29087765
+	2017	 234462			181689			77.4919				52773				22.5081				66020915			26861738
+	2018	 997497			759078			76.0983				238419				23.9017				307748862			105938301
+	2019	 847083			651750			76.9405				195333				23.0595				257563281			78455821
+	2020	 350240			261684			74.7156				88556				25.2844				113136780			29087765
 
 1. Over the years, we can say that, out of '100' products sold, 
 	on an Average '76' would be through "BRICK & MORTAR" stores & '24' would be through "E-COMMERCE" Channels.
@@ -407,29 +408,29 @@ WHERE Rnk<6 ;
 
 /*
 	Yearr	Rnk	customer_code	customer_name			customer_type		Revenue		Contribution_to_Total_Revenue_that_year
-	2017	1		Cus006		Electricalsara Stores	Brick & Mortar		36098419				38.86
-	2017	2		Cus020		Nixon					E-Commerce			7914695					8.52
-	2017	3		Cus022		Electricalslytical		E-Commerce			6135803					6.61
-	2017	4		Cus005		Premium Stores			Brick & Mortar		5237662					5.64
-	2017	5		Cus003		Excel Stores			Brick & Mortar		4600061					4.95
+	2017	1	   Cus006	Electricalsara Stores		Brick & Mortar		36098419			38.86
+	2017	2	   Cus020	Nixon				E-Commerce		7914695				8.52
+	2017	3	   Cus022	Electricalslytical		E-Commerce		6135803				6.61
+	2017	4	   Cus005	Premium Stores			Brick & Mortar		5237662				5.64
+	2017	5	   Cus003	Excel Stores			Brick & Mortar		4600061				4.95
 	
-    2018	1		Cus006		Electricalsara Stores   Brick & Mortar		173050977				41.83
-	2018	2		Cus020		Nixon					E-Commerce			21809554				5.27
-	2018	3		Cus022		Electricalslytical		E-Commerce			20825800				5.03
-	2018	4		Cus005		Premium Stores			Brick & Mortar		18693181				4.52
-	2018	5		Cus003		Excel Stores			Brick & Mortar		18051333				4.36
+        2018	1	   Cus006	Electricalsara Stores   	Brick & Mortar		173050977			41.83
+	2018	2	   Cus020	Nixon				E-Commerce		21809554			5.27
+	2018	3	   Cus022	Electricalslytical		E-Commerce		20825800			5.03
+	2018	4	   Cus005	Premium Stores			Brick & Mortar		18693181			4.52
+	2018	5	   Cus003	Excel Stores			Brick & Mortar		18051333			4.36
 	
-    2019	1		Cus006		Electricalsara Stores   Brick & Mortar		138542215				41.23
-	2019	2		Cus003		Excel Stores		    Brick & Mortar		18535841				5.52
-	2019	3		Cus022		Electricalslytical	    E-Commerce			17144682				5.1
-	2019	4		Cus005		Premium Stores			Brick & Mortar		15076075				4.49
-	2019	5		Cus007		Info Stores				Brick & Mortar		12569241				3.74
+    	2019	1	   Cus006	Electricalsara Stores   	Brick & Mortar		138542215			41.23
+	2019	2	   Cus003	Excel Stores		    	Brick & Mortar		18535841			5.52
+	2019	3	   Cus022	Electricalslytical	    	E-Commerce		17144682			5.1
+	2019	4	   Cus005	Premium Stores			Brick & Mortar		15076075			4.49
+	2019	5	   Cus007	Info Stores			Brick & Mortar		12569241			3.74
 	
-    2020	1		Cus006		Electricalsara Stores	Brick & Mortar		65641977				46.15
-	2020	2		Cus003		Excel Stores			Brick & Mortar		7928385					5.57
-	2020	3		Cus005		Premium Stores			Brick & Mortar		5899748					4.15
-	2020	4		Cus022		Electricalslytical		E-Commerce			5537904					3.89
-	2020	5		Cus007		Info Stores				Brick & Mortar		5064374					3.56
+    	2020	1	   Cus006	Electricalsara Stores		Brick & Mortar		65641977			46.15
+	2020	2	   Cus003	Excel Stores			Brick & Mortar		7928385				5.57
+	2020	3	   Cus005	Premium Stores			Brick & Mortar		5899748				4.15
+	2020	4	   Cus022	Electricalslytical		E-Commerce		5537904				3.89
+	2020	5	   Cus007	Info Stores			Brick & Mortar		5064374				3.56
 
 Thus, we see that :
 1. The list remains nearly consistent, with most pertinent customers being :
